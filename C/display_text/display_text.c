f664198117f450f1fb543ad1cc4141c2ca238e6e
shanelperera-sliit <it24101845@my.sliit.lk>
2024-07-27 12:42:21 +0530

    adding display_text

---
+++ C/display_text/display_text.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void) {
+    printf("Hello! Welcome to SLIIT");
+    printf("First line\nThis is the second line\n");
+    printf("   X   \n");
+    printf(" X   X \n");
+    printf("X     X\n");
+    printf(" X   X \n");
+    printf("X     X\n");
+    printf(" X   X \n");
+    printf("X     X\n");
+    printf(" X   X \n");
+    printf("   X   \n");
+    return 0;
+}
