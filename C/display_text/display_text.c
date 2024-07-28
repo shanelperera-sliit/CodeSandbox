e020309c543f7e83688e017563376d03868c0425
shanelperera-sliit <it24101845@my.sliit.lk>
2024-07-28 08:00:33 +0530

    changes to display_text

--- C/display_text/display_text.c
+++ C/display_text/display_text.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(void) {
-    printf("Hello! Welcome to SLIIT");
+    printf("Hello! Welcome to SLIIT\n");
     printf("First line\nThis is the second line\n");
     printf("   X   \n");
     printf(" X   X \n");
